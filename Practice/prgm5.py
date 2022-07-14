5b85863bcd077fdef4bdf3719f88e81f6c6dc4e9
Deepak Gurjar <78991392+deepak-gurjar07@users.noreply.github.com>
2022-07-14 11:39:43 +0530

    Create prgm5.py

---
+++ Practice/prgm5.py
@@ -0,0 +1,19 @@
+/*Chef has recently started playing chess, and wants to play as many games as possible.
+
+He calculated that playing one game of chess takes at least 2020 minutes of his time.
+
+Chef has NN hours of free time. What is the maximum number of complete chess games he can play in that time?*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n*3<<endl;
+    }
+    return 0;
+}
